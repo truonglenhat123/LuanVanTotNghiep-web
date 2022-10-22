933e2f729f32bcd50ad223508bf5e56964777dc4
Thanh Phan <thanh.phan@altsourcesoftware.com>
2022-10-22 11:30:25 +0700

    update

--- WebsiteBanDienThoai/Controllers/HomeController.cs
+++ WebsiteBanDienThoai/Controllers/HomeController.cs
@@ -3,14 +3,17 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using WebsiteBanDienThoai.Models;
 
 namespace WebsiteBanDienThoai.Controllers
 {
     public class HomeController : Controller
     {
+        dbKL4Entities db = new dbKL4Entities();
         public ActionResult Index()
         {
-            return View();
+            var listProductHome = db.SanPhams.ToList();
+            return View(listProductHome);
         }
 
     }
---
+++ WebsiteBanDienThoai/Controllers/ProductController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebsiteBanDienThoai.Models;
+
+namespace WebsiteBanDienThoai.Controllers
+{
+    public class ProductController : Controller
+    {
+        dbKL4Entities db = new dbKL4Entities();
+        // GET: Product
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult CategoryMenu()
+        {
+            var model = db.DanhMucs;
+            return PartialView("_Category", model);
+        }
+        public ActionResult ChiTietSanPham(int id)
+        {
+            var model = db.SanPhams.Where(n => n.maSP == id).FirstOrDefault();
+            return View(model);
+        }
+    }
+}
